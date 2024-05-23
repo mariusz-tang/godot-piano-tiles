cb47cd37973e0a2d9535626521fcaeed0a8f988a
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-05-23 20:44:53 +0100

    Add Main scene
    - Include tile generation and shifting using a placeholder input binding

---
+++ scripts/Main.gd
@@ -0,0 +1,58 @@
+class_name Main
+extends Node
+
+const _TILE_HEIGHT: int = 720 / 4
+const _TILE_WIDTH: int = 120
+
+@export var _tile_scene: PackedScene
+
+var _tiles: Array[Tile] = []
+var _tile_positions: Array[int] = []
+
+
+func _ready() -> void:
+	_generate_initial_tiles()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("shift_tiles"):
+		_shift()
+
+
+func _generate_initial_tiles() -> void:
+	while _tiles.size() == 0 or _tiles[-1].position.y > 0:
+		_generate_new_tile()
+
+
+func _generate_new_tile() -> void:
+	var tile: Tile = _tile_scene.instantiate()
+	add_child(tile)
+	tile.set_size(_TILE_WIDTH, _TILE_HEIGHT)
+
+	var position := randi() % 4
+	var x := position * _TILE_WIDTH
+
+	var last_tile_y_pos: int = 720
+	if _tiles.size() != 0:
+		last_tile_y_pos = _tiles[-1].position.y
+	var y := last_tile_y_pos - _TILE_HEIGHT
+
+	tile.position = Vector2(x, y)
+
+	_tiles.append(tile)
+	_tile_positions.append(position)
+
+
+func _shift() -> void:
+	var bottom_tile: Tile = _tiles.pop_front()
+	bottom_tile.queue_free()
+	_tile_positions.remove_at(0)
+
+	_generate_new_tile()
+	for tile_index in range(_tiles.size()):
+		var tile := _tiles[tile_index]
+		var tween := tile.create_tween()
+		var final_position := Vector2(
+			tile.position.x, 720 - _TILE_HEIGHT * (tile_index + 1)
+		)
+		tween.tween_property(tile, "position", final_position, 0.1)
