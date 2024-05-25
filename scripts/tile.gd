5ddf7e46ca18d9e25e68e22b8bd6f221cbadfbe7
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-05-25 16:01:05 +0100

    Remove unnecessary collider from tile scene
    Also changed the root node type from Area2D to Node2D since we no longer have a CollisionShape

--- scripts/tile.gd
+++ scripts/tile.gd
@@ -1,28 +1,15 @@
 class_name Tile
-extends Area2D
+extends Node2D
 ## Resizable piano tile
 
-@export var _collider: CollisionShape2D
 @export var _sprite: Sprite2D
-
-@onready var _collision_shape: RectangleShape2D = _collider.shape
-
-
-func _ready() -> void:
-	set_size(45, 100)
 
 
 ## Set the size of the tile in pixels.
 func set_size(x: int, y: int) -> void:
 	var new_size := Vector2(x, y)
-	var new_position := new_size / 2
-
-	_collision_shape.size = new_size
 	_sprite.scale = new_size
-
-	# Adjust the positions since the origin is in the centre of the rectangle.
-	_collider.position = new_position
-	_sprite.position = new_position
+	_sprite.position = new_size / 2
 
 
 func fade_and_free(tween: Tween) -> void:
