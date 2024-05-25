0d2dc7ab44ae25cc717f567a4ab4ed74b0624771
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-05-25 16:27:24 +0100

    Add start button to HUD

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,10 +1,18 @@
 class_name HUD
 extends CanvasLayer
 
+signal start_pressed
+
 @export var score_label: Label
 @export var highscore_label: Label
+@export var start_button: Button
 
 
 func update_scores(score: int, highscore: int) -> void:
 	score_label.text = str(score)
 	highscore_label.text = "Highscore: " + str(highscore)
+
+
+func _on_start_button_pressed() -> void:
+	start_button.hide()
+	start_pressed.emit()
