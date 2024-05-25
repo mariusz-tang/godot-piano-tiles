8ba63f81e8d03bad55a385ce8a882782136ec19f
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-05-25 17:08:15 +0100

    Refactor gameplay logic to its own node

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,19 +2,18 @@ class_name Main
 extends Node
 
 signal score_changed(new_score: int, new_highscore: int)
+signal game_over
 
 const _TILE_HEIGHT: int = 720 / 4
 const _TILE_WIDTH: int = 120
 const _TILE_ACTIONS: Array[String] = ["tile_1", "tile_2", "tile_3", "tile_4"]
 
 @export var _hud: HUD
-@export var _tile_scene: PackedScene
+@export var _game: TilePlayer
 @export var _tile_sound_player: AudioStreamPlayer2D
 @export var _success_sound: AudioStream
 @export var _fail_sound: AudioStream
 
-var _tiles: Array[Tile] = []
-var _tile_positions: Array[int] = []
 var _score: int = 0:
 	get:
 		return _score
@@ -26,63 +25,29 @@ var _highscore: int = 0
 
 
 func _ready() -> void:
-	_generate_initial_tiles()
 	score_changed.connect(_hud.update_scores)
 
-
-func _input(event: InputEvent) -> void:
-	for action_ix in range(_TILE_ACTIONS.size()):
-		if event.is_action_pressed(_TILE_ACTIONS[action_ix]):
-			_handle_tile_pressed(action_ix)
+	_game.good_press.connect(_add_point)
+	_game.bad_press.connect(_game_over)
+	_game.generate()
 
 
-func _generate_initial_tiles() -> void:
-	while _tiles.size() == 0 or _tiles[-1].position.y > 0:
-		_generate_new_tile()
+func _start_game() -> void:
+	_game.play(true)
 
 
-func _generate_new_tile() -> void:
-	var tile: Tile = _tile_scene.instantiate()
-	add_child(tile)
-	tile.set_size(_TILE_WIDTH, _TILE_HEIGHT)
-
-	var position := randi() % 4
-	var x := position * _TILE_WIDTH
-
-	var last_tile_y_pos: int = 720
-	if _tiles.size() != 0:
-		last_tile_y_pos = _tiles[-1].position.y
-	var y := last_tile_y_pos - _TILE_HEIGHT
-
-	tile.position = Vector2(x, y)
-
-	_tiles.append(tile)
-	_tile_positions.append(position)
+func _add_point() -> void:
+	_score += 1
+	_play_sound(_success_sound)
 
 
-func _handle_tile_pressed(index: int) -> void:
-	assert(index >= 0 and index <= 3)
-	if _tile_positions[0] == index:
-		_shift()
-		_tile_sound_player.stream = _success_sound
-		_score += 1
-	else:
-		_tile_sound_player.stream = _fail_sound
-		_score = 0
+func _game_over() -> void:
+	_score = 0
+	_game.pause()
+	_play_sound(_fail_sound)
+	game_over.emit()
+
+
+func _play_sound(sound: AudioStream) -> void:
+	_tile_sound_player.stream = sound
 	_tile_sound_player.play()
-
-
-func _shift() -> void:
-	_generate_new_tile()
-	for tile_index in range(_tiles.size()):
-		var tile := _tiles[tile_index]
-		var tween := tile.create_tween()
-		var final_position := Vector2(
-			tile.position.x, 720 - _TILE_HEIGHT * tile_index
-		)
-		tween.tween_property(tile, "position", final_position, 0.1)
-		if tile_index == 0:
-			tile.shrink_and_free(tween)
-
-	_tiles.remove_at(0)
-	_tile_positions.remove_at(0)
---
+++ scripts/tile_player.gd
@@ -0,0 +1,100 @@
+class_name TilePlayer
+extends Node
+## A gameplay manager class. Handles tile generation and parses gameplay inputs.
+
+## Emitted when the correct tile is pressed.
+signal good_press
+## Emitted when an incorrect tile is pressed.
+signal bad_press
+
+const _TILE_HEIGHT: int = 720 / 4
+const _TILE_WIDTH: int = 120
+const _TILE_ACTIONS: Array[String] = ["tile_1", "tile_2", "tile_3", "tile_4"]
+
+@export var _tile_scene: PackedScene
+
+var _tiles: Array[Tile] = []
+var _tile_positions: Array[int] = []
+
+
+func _ready() -> void:
+	# Begin in a paused state until play is called.
+	set_process_unhandled_input(false)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	for action_ix in range(_TILE_ACTIONS.size()):
+		if event.is_action_pressed(_TILE_ACTIONS[action_ix]):
+			_handle_tile_pressed(action_ix)
+			get_viewport().set_input_as_handled()
+
+
+## Clear any existing tiles and generate a new set.
+func generate() -> void:
+	while _tiles.size() > 0:
+		var tile: Tile = _tiles.pop_back()
+		tile.queue_free()
+	_tile_positions.clear()
+	_generate_initial_tiles()
+
+
+## Enable input processing. If [param regenerate] is set to [code]true[/code],
+## [method TilePlayer.generate] is called first.
+func play(regenerate: bool = false) -> void:
+	if regenerate:
+		generate()
+	set_process_unhandled_input(true)
+
+
+## Disable input processing. Call [method TilePlayer.play] to unpause.
+func pause() -> void:
+	set_process_unhandled_input(false)
+
+
+func _generate_initial_tiles() -> void:
+	while _tiles.size() == 0 or _tiles[-1].position.y > 0:
+		_generate_new_tile()
+
+
+func _generate_new_tile() -> void:
+	var tile: Tile = _tile_scene.instantiate()
+	add_child(tile)
+	tile.set_size(_TILE_WIDTH, _TILE_HEIGHT)
+
+	var tile_x_pos := randi() % 4
+	var x := tile_x_pos * _TILE_WIDTH
+
+	var last_tile_y_pos: int = 720
+	if _tiles.size() != 0:
+		last_tile_y_pos = _tiles[-1].position.y
+	var y := last_tile_y_pos - _TILE_HEIGHT
+
+	tile.position = Vector2(x, y)
+
+	_tiles.append(tile)
+	_tile_positions.append(tile_x_pos)
+
+
+func _handle_tile_pressed(index: int) -> void:
+	assert(index >= 0 and index <= 3)
+	if _tile_positions[0] == index:
+		_shift()
+		good_press.emit()
+	else:
+		bad_press.emit()
+
+
+func _shift() -> void:
+	_generate_new_tile()
+	for tile_index in range(_tiles.size()):
+		var tile := _tiles[tile_index]
+		var tween := tile.create_tween()
+		var final_position := Vector2(
+			tile.position.x, 720 - _TILE_HEIGHT * tile_index
+		)
+		tween.tween_property(tile, "position", final_position, 0.1)
+		if tile_index == 0:
+			tile.shrink_and_free(tween)
+
+	_tiles.remove_at(0)
+	_tile_positions.remove_at(0)
